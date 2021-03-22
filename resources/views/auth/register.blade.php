f7e345f54933e99c358eba86806a4205986f311c
bahri <bahrimohamedamin7@gmail.com>
2021-03-22 04:03:53 +0100

    last commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,30 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="roles" class="col-md-4 col-form-label text-md-right">{{ __('Role') }}</label>
+
+                            <div class="col-md-6">
+                                <select name="roles" required class="form-control">
+                                    <option value="" selected disabled>
+                                        Choisir un role
+                                    </option>
+                                    <option value="admin">
+                                        admin
+                                    </option>
+                                    <option value="user">
+                                        user
+                                    </option>
+                                    <option value="auteur">
+                                        auteu
+                                    </option>
+                                    <option value="superviseur">
+                                        superviseur
+                                    </option>
+                                </select>
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
